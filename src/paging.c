1732bbba6bcab0e6a9fbaa95431cbc05e71dcd2f
egd0r <e.veigarianne@lancaster.ac.uk>
2022-11-29 09:07:49 +0000

    Written unmapping paging and solved mystery of weird 0x20 (accessed bit)

--- src/paging.c
+++ src/paging.c
@@ -1,13 +1,18 @@
-#define PAGE_DIR_VIRT 0xFFFFFFFFFFE00000 // Last entry in L1 maps to L4, indexing this address gives PDEs
-
+#define PAGE_DIR_VIRT 0xFFFFFFFFBFFFF000 // Last entry in L1 maps to L4, indexing this address gives PDEs
+                    //0xFFFFFFFFC0000000
 /*                                L3        L2        L1          
     Page Dir Virt = 0xFFFF  1111 1111 1111 1111 1111 1111 11    
                              F    F   F     F   F     F    C
     Page Dir itself is mapped to a 4kB page
 */                  
 
+#define PRESENT     0x01
+#define RW          0x02
+#define HUGE_PAGE   0x80
+#define FLAGS       0xFFF
 
-void get_physaddr(void *virt_addr) {                         //0xFFFFFFFFC0000000
+
+void get_physaddr(void *virt_addr) {                         
     // unsigned long pdindex = (unsigned long)virt_addr >> 39   & 0x0000FF8000000000;
     // unsigned long ptl3index = (unsigned long)virt_addr >> 30 & 0x0000007FC0000000;
     // unsigned long ptl2index = (unsigned long)virt_addr >> 21 & 0x000000002FE00000;
@@ -24,17 +29,38 @@ void get_physaddr(void *virt_addr) {                         //0xFFFFFFFFC000000
 
 }
 
-void *unmap_page(unsigned long long virt_addr) {
+void unmap_page(unsigned long long virt_addr) {
 
     // Deconstruct virtual address
     // Index page directory to check mapping
 
-    unsigned long long pdIndex = ((unsigned long)virt_addr & 0x0000FF8000000000) >> 39;
+    uint16_t pdIndex = ((unsigned long)virt_addr & 0x0000FF8000000000) >> 39; // Max 9 bits
 
-    unsigned long long *pd = (unsigned long long *)PAGE_DIR_VIRT;
 
+    // unsigned long long *pd = (unsigned long long *)PAGE_DIR_VIRT;
     // unsigned long long ptr = pd[pdIndex]; // Check flags on this ptr FAULT
+    
+    
+    // unsigned long long l3index = ((unsigned long)virt_addr & 0x0000007FC0000000) >> 30;
 
 
+    uint64_t pd = (uint64_t *)PAGE_DIR_VIRT;
+    uint16_t flags;
+    do {
+        pd = ((uint64_t *)(pd & ~FLAGS))[pdIndex]; // Deref masked out flags
+    } while ((pd & PRESENT) && !(pd & HUGE_PAGE));
+
+    if (!(pd & PRESENT)) {
+        // printf("This entry is not present!");
+        return;
+    }
+
+    pd &= (~PRESENT); //Removing present flag, invalidating entry
+
+    return;
+
+}
+
+void *allocate_page() { // Walk pages and return address of free page?
 
 }
